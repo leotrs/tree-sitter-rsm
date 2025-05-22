ed94b0ca2a7a951a04f3c1c9a76501d8cc51227c
Leo Torres <leo.torres@fgsglobal.com>
2025-05-22 21:41:10 +0200

    use ABI 14

--- src/parser.c
+++ src/parser.c
@@ -14,7 +14,7 @@
 #pragma GCC optimize ("O0")
 #endif
 
-#define LANGUAGE_VERSION 15
+#define LANGUAGE_VERSION 14
 #define STATE_COUNT 638
 #define LARGE_STATE_COUNT 82
 #define SYMBOL_COUNT 178
@@ -4355,7 +4355,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 455},
   [2] = {.lex_state = 455, .external_lex_state = 2},
@@ -26867,7 +26867,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_rsm(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
-    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -26894,13 +26893,6 @@ TS_PUBLIC const TSLanguage *tree_sitter_rsm(void) {
       tree_sitter_rsm_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
-    .name = "rsm",
-    .max_reserved_word_set_size = 0,
-    .metadata = {
-      .major_version = 0,
-      .minor_version = 1,
-      .patch_version = 6,
-    },
   };
   return &language;
 }
